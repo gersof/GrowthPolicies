bcdf7fc7963bec35d05c023fba25be2306330290
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2019-03-30 14:32:11 -0500

    I did create a DbContext

--- 0_ServerApp/GrowthPolicies/GrowthPolicies.DataAccess/ApplicationDbContext.cs
+++ 0_ServerApp/GrowthPolicies/GrowthPolicies.DataAccess/ApplicationDbContext.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GrowthPolicies.DataAccess.Configuration;
+using GrowthPolicies.Models.AccountModels;
+using GrowthPolicies.Models.PolicesModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace GrowthPolicies.DataAccess
 {
-    public class ApplicationDbContext
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        private const string ConnectionString = "GrowthPoliciesConnection";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbSet<PolicyModel> Policies { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbSet<CoverageModel> Coverages { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbSet<RiskModel> Risks { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbSet<UserPolicyModel> UserPolicies { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ApplicationDbContext()
+            : base(ConnectionString, throwIfV1Schema: false)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new PolicyConfiguration());
+            modelBuilder.Configurations.Add(new CoverageConfiguration());
+            modelBuilder.Configurations.Add(new RiskConfiguration());
+            modelBuilder.Configurations.Add(new UserPolicyConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
+   
