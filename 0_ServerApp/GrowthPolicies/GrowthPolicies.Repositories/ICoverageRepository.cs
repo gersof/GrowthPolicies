f2ed78ba701a25b7b355cd630fb3c8b15905431c
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2019-03-30 11:20:49 -0500

    Added Coverage Interface

---
+++ 0_ServerApp/GrowthPolicies/GrowthPolicies.Repositories/ICoverageRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrowthPolicies.Models.PolicesModels;
+
+namespace GrowthPolicies.Repositories
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICoverageRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<CoverageModel> GetCoverages();
+    }
+}
