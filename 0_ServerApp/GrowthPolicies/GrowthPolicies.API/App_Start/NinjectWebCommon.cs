d1b99f543906386f423b8fd4eccc712de0ba0d09
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2019-03-30 19:19:30 -0500

    Added Ninject and create NinjectWebCommon

---
+++ 0_ServerApp/GrowthPolicies/GrowthPolicies.API/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,60 @@
+﻿using Ninject.Web.Common.WebHost;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(GrowthPolicies.API.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(GrowthPolicies.API.App_Start.NinjectWebCommon), "Stop")]
+
+namespace GrowthPolicies.API.App_Start
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Ninject;
+    using Ninject.Extensions.Conventions;
+    using Ninject.Web.Common;
+    using System;
+    using System.Web;
+
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+        
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                kernel.Bind(x =>
+                {
+                    x.FromThisAssembly()
+                        .SelectAllClasses()
+                        .BindDefaultInterface();
+                });
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+        private static void RegisterServices(IKernel kernel)
+        {
+        }
+    }
+}
