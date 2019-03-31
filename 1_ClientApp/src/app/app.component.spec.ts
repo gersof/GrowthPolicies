12a60f6f4883e88d334c538dd01b4715081508ea
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2019-03-31 08:25:38 -0500

    Scaffold for initial Angular APP with Angular CLI

--- 1_ClientApp/src/app/app.component.spec.ts
+++ 1_ClientApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'growthpolicesApp'`, () => {
+  it(`should have as title 'GrowthPolicesAPP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('growthpolicesApp');
+    expect(app.title).toEqual('GrowthPolicesAPP');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to growthpolicesApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to GrowthPolicesAPP!');
   });
 });
--- 1_ClientApp/src/karma.conf.js
+++ 1_ClientApp/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/growthpolicesApp'),
+      dir: require('path').join(__dirname, '../coverage/GrowthPolicesAPP'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
